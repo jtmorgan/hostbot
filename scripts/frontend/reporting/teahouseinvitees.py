b1c14d6fe81d0dd54657adad4efd2ba3fd3fc7a3
Jmorgan <jmorgan@tools.wmflabs.org>
2013-10-28 20:30:04 +0000

    reconfigured host ranking process

--- scripts/frontend/reporting/teahouseinvitees.py
+++ scripts/frontend/reporting/teahouseinvitees.py
@@ -86,7 +86,7 @@ FROM enwiki_p.user
 WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 1 DAY),'%Y%m%d%H%i%s')
 AND user_editcount > 10
 AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
-AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging where log_type = "block" and log_action = "block" and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s'));
+AND user_name not in (SELECT log_title from enwiki_p.logging where log_type = "block" and log_action = "block" and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s'));
 ''')
 conn.commit()
 
---
+++ scripts/frontend/updating/rankHostProfiles.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python2.7
+
+# Copyright 2013 Jtmorgan
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import MySQLdb
+import profiles
+import hostbot_settings
+import output_settings
+import sys
+
+###FUNCTIONS###
+def rankProfiles():
+	"""
+	rank Eval portal profiles by number of recent edits.
+	"""
+	profile_page = profiles.Profiles(params['output path'], params['output page id'], params)
+	profile_list = profile_page.getPageSectionData("2")
+	profile_list = tools.dedupeMemberList(profile_list, "index", "title")
+	quote1 = "'"
+	quote2 = "'"
+	profile_names = [x['title'] for x in profile_list]	
+	query_list = quote1 + "','".join(name for name in profile_names) + quote2	
+	query = "SELECT user_name, num_edits_2wk FROM th_up_hosts WHERE user_name IN (%s);" % query_list	
+# 	print query
+# 	print type(query)
+	cursor.execute(query)
+	rows = cursor.fetchall()
+	output = [{'edits' : row[1], 'title' : row[0].decode("utf8")} for row in rows]
+	for p in profile_list:
+		p['text'] = profile_page.getPageText(p['index'])
+		p['edits'] = [o['edits'] for o in output if o['title'] == p['title']]
+	for p in profile_list:
+		if 'edits' not in p:
+			p['edits'] = 0
+		else:
+			pass
+	missing_profiles = [o['title'] for o in output if o not in profile_names]			
+# 	print missing_profiles
+	plist_sorted = sorted(profile_list, key=lambda item: item['edits'], reverse = True)
+	plist_text = {'profiles' : '\n'.join([x['text'] for x in plist_sorted])} 
+	formatted_profiles = profile_page.formatProfile(plist_text)
+	edit_summ = params['edit summary'] % (params['type'],)
+	profile_page.publishProfile(formatted_profiles, params['output path'], edit_summ, edit_sec = 1)
+
+
+###MAIN###
+conn = MySQLdb.connect(host = hostbot_settings.host, db = hostbot_settings.dbname, read_default_file = hostbot_settings.defaultcnf, use_unicode=True, charset="utf8")
+cursor = conn.cursor()
+param = output_settings.Params()
+params = param.getParams(sys.argv[1])
+params['type'] = sys.argv[1]
+tools = profiles.Toolkit()
+rankProfiles()
+cursor.close()
+conn.close()
