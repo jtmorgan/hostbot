4d7e245ff1dc91abde0cb15bdfc2b41051c7de7f
Jonathan Morgan <jonnymorgan.esq@gmail.com>
2016-02-22 00:55:46 +0000

    rename tools module descriptively

--- scripts/teahouse/send_th_invites.py
+++ scripts/teahouse/send_th_invites.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import hb_hosts
+import hb_toolkit
 import hb_output_settings
 import hb_profiles
 import random
