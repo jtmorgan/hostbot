cb9fd3a47fd657621609beab7b78128bef14f00a
Jonathan Morgan <jonnymorgan.esq@gmail.com>
2018-03-18 21:33:16 +0000

    detab, new db details, nobots compliance

--- hb_output_settings.py
+++ hb_output_settings.py
@@ -1,59 +1,59 @@
 #! /usr/bin/env python
 
 class Params:
-	"""
-	output parameters for running the invite script under different conditions.
-	"""
+    """
+    output parameters for running the invite script under different conditions.
+    """
 
-	def __init__(self):
-		self.output_params = {
-			'th_invites' : {
-			    'insert query' : 'teahouse experiment newbies',
-			    'select query' : 'th experiment invitees',
-			    'status update query' : 'update th invite status',
-			    'talkpage update query' : 'th add talkpage',
-				'type' : 'th invite templates',
-				'output namespace' : 'User_talk:',
-				'output section' : 'new',
-				'sample size' : 300,
-				'edit summary' : ', you are invited to the Teahouse!',
-				'output section title' : '== {{subst:PAGENAME}}, you are invited to the Teahouse ==',
-				'inviters' : ['Rosiestep','Jtmorgan','Missvain','Liz','Naypta','AmaryllisGardener','Doctree','I JethroBT', 'Dathus', 'Cordless Larry', 'Gestrid', 'Cullen328', 'Lectonar', 'Mz7', '78.26', 'Worm That Turned', 'ChamithN', 'Samwalton9',],
-				'conditions' : ['th-invite'],
-				'skip templates' : ['uw-vandalism4', 'final warning', '{{sock|', 'uw-unsourced4', 'uw-socksuspect', 'Socksuspectnotice', 'only warning','without further warning', 'Uw-socksuspect', 'sockpuppetry', 'Teahouse', 'uw-cluebotwarning4', 'uw-vblock', 'uw-speedy4'],
-				},
-			'test_invites' : { #when using the test db th_invite_test
-			    'insert query' : 'teahouse test',
-			    'select query' : 'th test invitees',
-			    'status update query' : 'update test invite status',
-			    'talkpage update query' : 'th add talkpage test',
-				'type' : 'test invite templates',
-				'output namespace' : 'User_talk:',
-				'output section' : 'new',
-				'sample size' : 300,
-				'edit summary' : ', you are invited to the Teahouse!',
-				'output section title' : '== {{subst:PAGENAME}}, you are invited to the Teahouse ==',
-				'inviters' : ['Rosiestep','Jtmorgan','Missvain','Liz','Naypta','AmaryllisGardener','Doctree','I JethroBT', 'Dathus', 'Cordless Larry', 'Gestrid', 'Cullen328', 'Lectonar', 'Mz7', '78.26', 'Worm That Turned', 'ChamithN', 'Samwalton9',],
-				'conditions' : ['th-invite'],
-				'skip templates' : ['uw-vandalism4', 'final warning', '{{sock|', 'uw-unsourced4', 'uw-socksuspect', 'Socksuspectnotice', 'only warning','without further warning', 'Uw-socksuspect', 'sockpuppetry', 'Teahouse', 'uw-cluebotwarning4', 'uw-vblock', 'uw-speedy4'],
-				},
-			'training_module_invites' : {
-			    'insert query' : 'tm insert records',
-			    'select query' : 'tm select records',
-			    'status update query' : 'tm update invite status',
-			    'talkpage update query' : 'tm add talkpage',
-				'type' : 'tm invite template',
-				'output namespace' : 'User_talk:',
-				'output section' : 'new',
-				'sample size' : 400,
-				'edit summary' : ', you are invited to try Wikipedia training modules',
-				'output section title' : '== {{subst:PAGENAME}}, you are invited to try training modules ==',
-				'inviters' : ['NULL'], #make sure this is ignored when formatting template
-				'conditions' : ['tm-invite', 'control'],
-				'skip templates' : ['uw-vandalism4', 'final warning', '{{sock|', 'uw-unsourced4', 'uw-socksuspect', 'Socksuspectnotice', 'only warning','without further warning', 'Uw-socksuspect', 'sockpuppetry', 'uw-cluebotwarning4', 'uw-vblock', 'uw-speedy4'],
-				},
-			}
+    def __init__(self):
+        self.output_params = {
+            'th_invites' : {
+                'insert query' : 'teahouse experiment newbies',
+                'select query' : 'th experiment invitees',
+                'status update query' : 'update th invite status',
+                'talkpage update query' : 'th add talkpage',
+                'type' : 'th invite templates',
+                'output namespace' : 'User_talk:',
+                'output section' : 'new',
+                'sample size' : 300,
+                'edit summary' : ', you are invited to the Teahouse!',
+                'output section title' : '== {{subst:PAGENAME}}, you are invited to the Teahouse ==',
+                'inviters' : ['Rosiestep','Jtmorgan','Missvain','Liz','Naypta','AmaryllisGardener','Doctree','I JethroBT', 'Dathus', 'Cordless Larry', 'Gestrid', 'Cullen328', 'Lectonar', 'Mz7', '78.26', 'Worm That Turned', 'ChamithN', 'Samwalton9',],
+                'conditions' : ['th-invite'],
+                'skip templates' : ['uw-vandalism4', 'final warning', '{{sock|', 'uw-unsourced4', 'uw-socksuspect', 'Socksuspectnotice', 'only warning','without further warning', 'Uw-socksuspect', 'sockpuppetry', 'Teahouse', 'uw-cluebotwarning4', 'uw-vblock', 'uw-speedy4', '{{bots|deny=HostBot','{{Bots|deny=HostBot','{{nobots','{{Nobots'],
+                },
+            'test_invites' : { #when using the test db th_invite_test
+                'insert query' : 'teahouse test',
+                'select query' : 'th test invitees',
+                'status update query' : 'update test invite status',
+                'talkpage update query' : 'th add talkpage test',
+                'type' : 'test invite templates',
+                'output namespace' : 'User_talk:',
+                'output section' : 'new',
+                'sample size' : 300,
+                'edit summary' : ', you are invited to the Teahouse!',
+                'output section title' : '== {{subst:PAGENAME}}, you are invited to the Teahouse ==',
+                'inviters' : ['Rosiestep','Jtmorgan','Missvain','Liz','Naypta','AmaryllisGardener','Doctree','I JethroBT', 'Dathus', 'Cordless Larry', 'Gestrid', 'Cullen328', 'Lectonar', 'Mz7', '78.26', 'Worm That Turned', 'ChamithN', 'Samwalton9',],
+                'conditions' : ['th-invite'],
+                'skip templates' : ['uw-vandalism4', 'final warning', '{{sock|', 'uw-unsourced4', 'uw-socksuspect', 'Socksuspectnotice', 'only warning','without further warning', 'Uw-socksuspect', 'sockpuppetry', 'Teahouse', 'uw-cluebotwarning4', 'uw-vblock', 'uw-speedy4', '{{bots|deny=HostBot','{{Bots|deny=HostBot','{{nobots','{{Nobots'],
+                },
+            'training_module_invites' : {
+                'insert query' : 'tm insert records',
+                'select query' : 'tm select records',
+                'status update query' : 'tm update invite status',
+                'talkpage update query' : 'tm add talkpage',
+                'type' : 'tm invite template',
+                'output namespace' : 'User_talk:',
+                'output section' : 'new',
+                'sample size' : 400,
+                'edit summary' : ', you are invited to try Wikipedia training modules',
+                'output section title' : '== {{subst:PAGENAME}}, you are invited to try training modules ==',
+                'inviters' : ['NULL'], #make sure this is ignored when formatting template
+                'conditions' : ['tm-invite', 'control'],
+                'skip templates' : ['uw-vandalism4', 'final warning', '{{sock|', 'uw-unsourced4', 'uw-socksuspect', 'Socksuspectnotice', 'only warning','without further warning', 'Uw-socksuspect', 'sockpuppetry', 'Teahouse', 'uw-cluebotwarning4', 'uw-vblock', 'uw-speedy4', '{{bots|deny=HostBot','{{Bots|deny=HostBot','{{nobots','{{Nobots'],
+                },
+            }
 
-	def getParams(self, profile_type):
-		tp = self.output_params[profile_type]
-		return tp
+    def getParams(self, profile_type):
+        tp = self.output_params[profile_type]
+        return tp
--- hb_queries.py
+++ hb_queries.py
@@ -1,115 +1,115 @@
 #! /usr/bin/env python
 
 class Query:
-	"""queries for database tracking tables"""
+    """queries for database tracking tables"""
 
-	def __init__(self):
-		self.mysql_queries = {
+    def __init__(self):
+        self.mysql_queries = {
 'teahouse experiment newbies' : {
     'string' : """insert ignore into th_up_invitees_current
-	(user_id, user_name, user_registration, user_editcount, sample_date, sample_type)
-	SELECT user_id, user_name, user_registration, user_editcount, NOW(), 4
-	FROM enwiki_p.user
-	WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s')
-	AND user_editcount >=5
-	AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
-	AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging
-		where log_type = "block" and log_action = "block"
-		and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s'))""",
-		        },
+    (user_id, user_name, user_registration, user_editcount, sample_date, sample_type)
+    SELECT user_id, user_name, user_registration, user_editcount, NOW(), 4
+    FROM enwiki_p.user
+    WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s')
+    AND user_editcount >=5
+    AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
+    AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging
+        where log_type = "block" and log_action = "block"
+        and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s'))""",
+                },
 'teahouse test' : { #when using the test db th_invite_test
     'string' : """insert ignore into th_invite_test
-	(user_id, user_name, user_registration, user_editcount, sample_date, sample_type)
-	SELECT user_id, user_name, user_registration, user_editcount, NOW(), 4
-	FROM enwiki_p.user
-	WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s')
-	AND user_editcount >=5
-	AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
-	AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging
-		where log_type = "block" and log_action = "block"
-		and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s'))""",
-		        },
+    (user_id, user_name, user_registration, user_editcount, sample_date, sample_type)
+    SELECT user_id, user_name, user_registration, user_editcount, NOW(), 4
+    FROM enwiki_p.user
+    WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s')
+    AND user_editcount >=5
+    AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
+    AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging
+        where log_type = "block" and log_action = "block"
+        and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 2 DAY),'%Y%m%d%H%i%s'))""",
+                },
 'tm insert records' : {
     'string' : """insert ignore into tm_up_invitees
-	(user_id, user_name, user_registration, user_editcount, sample_date, sample_type)
-	SELECT user_id, user_name, user_registration, user_editcount, NOW(), 1
-	FROM enwiki_p.user
-	WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 1 DAY),'%Y%m%d%H%i%s')
-	AND user_editcount >=2
-	AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
-	AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging
-		where log_type = "block" and log_action = "block"
-		and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 1 DAY),'%Y%m%d%H%i%s'))""",
-		        },
+    (user_id, user_name, user_registration, user_editcount, sample_date, sample_type)
+    SELECT user_id, user_name, user_registration, user_editcount, NOW(), 1
+    FROM enwiki_p.user
+    WHERE user_registration > DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 1 DAY),'%Y%m%d%H%i%s')
+    AND user_editcount >=2
+    AND user_id NOT IN (SELECT ug_user FROM enwiki_p.user_groups WHERE ug_group = 'bot')
+    AND user_name not in (SELECT REPLACE(log_title,"_"," ") from enwiki_p.logging
+        where log_type = "block" and log_action = "block"
+        and log_timestamp >  DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 1 DAY),'%Y%m%d%H%i%s'))""",
+                },
 'tm select records' : { #need to make sure I'm not selecting skips
-	'string' : """SELECT user_name, user_id, user_talkpage
-		FROM tm_up_invitees
-		WHERE date(sample_date) = date(NOW())
-		AND sample_type = 1
-		AND invite_status IS NULL
-		AND (ut_is_redirect = 0 OR ut_is_redirect IS NULL)""",
-				},
+    'string' : """SELECT user_name, user_id, user_talkpage
+        FROM tm_up_invitees
+        WHERE date(sample_date) = date(NOW())
+        AND sample_type = 1
+        AND invite_status IS NULL
+        AND (ut_is_redirect = 0 OR ut_is_redirect IS NULL)""",
+                },
 'th experiment invitees' : {
-	'string' : """SELECT user_name, user_id, user_talkpage
-		FROM th_up_invitees_current
-		WHERE date(sample_date) = date(NOW())
-		AND sample_type = 4
-		AND invite_status IS NULL
-		AND (ut_is_redirect = 0 OR ut_is_redirect IS NULL)""",
-				},
+    'string' : """SELECT user_name, user_id, user_talkpage
+        FROM th_up_invitees_current
+        WHERE date(sample_date) = date(NOW())
+        AND sample_type = 4
+        AND invite_status IS NULL
+        AND (ut_is_redirect = 0 OR ut_is_redirect IS NULL)""",
+                },
 'th test invitees' : { #when using the test db th_invite_test
-	'string' : """SELECT user_name, user_id, user_talkpage
-		FROM th_invite_test
-		WHERE date(sample_date) = date(NOW())
-		AND sample_type = 4
-		AND invite_status IS NULL
-		AND (ut_is_redirect = 0 OR ut_is_redirect IS NULL)""",
-				},
+    'string' : """SELECT user_name, user_id, user_talkpage
+        FROM th_invite_test
+        WHERE date(sample_date) = date(NOW())
+        AND sample_type = 4
+        AND invite_status IS NULL
+        AND (ut_is_redirect = 0 OR ut_is_redirect IS NULL)""",
+                },
 'update th invite status' : {
-	'string' : """update th_up_invitees_current set sample_group = '%s', invite_status = %d,  hostbot_skipped = %d where user_id = %d""",
-				},
+    'string' : """update th_up_invitees_current set sample_group = '%s', invite_status = %d,  hostbot_skipped = %d where user_id = %d""",
+                },
 'update test invite status' : { #when using the test db th_invite_test
-	'string' : """update th_invite_test set sample_group = '{group:s}', invite_status = {status:d},  hostbot_skipped = {skipped:d} where user_id = {user_id:d}""",
-				},
+    'string' : """update th_invite_test set sample_group = '{group:s}', invite_status = {status:d},  hostbot_skipped = {skipped:d} where user_id = {user_id:d}""",
+                },
 'tm update invite status' : {
-	'string' : """update tm_up_invitees set sample_group = '%s', invite_status = %d,  hostbot_skipped = %d where user_id = %d""",
-				},
+    'string' : """update tm_up_invitees set sample_group = '%s', invite_status = %d,  hostbot_skipped = %d where user_id = %d""",
+                },
 'th add talkpage' : {#should pull this from the API, because of replag
-	'string' : """UPDATE th_up_invitees_current as i, enwiki_p.page as p
-		SET i.user_talkpage = p.page_id, i.ut_is_redirect = p.page_is_redirect
-		WHERE date(i.sample_date) = date(NOW())
-		AND p.page_namespace = 3
-		AND REPLACE(i.user_name, " ", "_") = p.page_title
-		AND i.user_talkpage IS NULL""",
-				},
+    'string' : """UPDATE th_up_invitees_current as i, enwiki_p.page as p
+        SET i.user_talkpage = p.page_id, i.ut_is_redirect = p.page_is_redirect
+        WHERE date(i.sample_date) = date(NOW())
+        AND p.page_namespace = 3
+        AND REPLACE(i.user_name, " ", "_") = p.page_title
+        AND i.user_talkpage IS NULL""",
+                },
 'th add talkpage test' : { #when using the test db th_invite_test
-	'string' : """UPDATE th_invite_test as i, enwiki_p.page as p
-		SET i.user_talkpage = p.page_id, i.ut_is_redirect = p.page_is_redirect
-		WHERE date(i.sample_date) = date(NOW())
-		AND p.page_namespace = 3
-		AND REPLACE(i.user_name, " ", "_") = p.page_title
-		AND i.user_talkpage IS NULL""",
-				},
+    'string' : """UPDATE th_invite_test as i, enwiki_p.page as p
+        SET i.user_talkpage = p.page_id, i.ut_is_redirect = p.page_is_redirect
+        WHERE date(i.sample_date) = date(NOW())
+        AND p.page_namespace = 3
+        AND REPLACE(i.user_name, " ", "_") = p.page_title
+        AND i.user_talkpage IS NULL""",
+                },
 'tm add talkpage' : {
-	'string' : """UPDATE tm_up_invitees as i, enwiki_p.page as p
-		SET i.user_talkpage = p.page_id, i.ut_is_redirect = p.page_is_redirect
-		WHERE date(i.sample_date) = date(NOW())
-		AND p.page_namespace = 3
-		AND REPLACE(i.user_name, " ", "_") = p.page_title
-		AND i.user_talkpage IS NULL""",
-				},
+    'string' : """UPDATE tm_up_invitees as i, enwiki_p.page as p
+        SET i.user_talkpage = p.page_id, i.ut_is_redirect = p.page_is_redirect
+        WHERE date(i.sample_date) = date(NOW())
+        AND p.page_namespace = 3
+        AND REPLACE(i.user_name, " ", "_") = p.page_title
+        AND i.user_talkpage IS NULL""",
+                },
 }
 
-	def getQuery(self, query_type, query_vars = False):
-		if query_type in self.mysql_queries:
-# 			query = self.mysql_queries[query_type]['string'].encode("utf8")
-			query = self.mysql_queries[query_type]['string']
-			if query_vars:
-# 				query = query % tuple(query_vars) #should accept a list containing any number of vars
-				query = query.format(**query_vars)
+    def getQuery(self, query_type, query_vars = False):
+        if query_type in self.mysql_queries:
+#           query = self.mysql_queries[query_type]['string'].encode("utf8")
+            query = self.mysql_queries[query_type]['string']
+            if query_vars:
+#               query = query % tuple(query_vars) #should accept a list containing any number of vars
+                query = query.format(**query_vars)
 
-			else:
-				pass
-			return query
-		else:
-			print("something went wrong with query of type " + query_type)
+            else:
+                pass
+            return query
+        else:
+            print("something went wrong with query of type " + query_type)
