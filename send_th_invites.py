d424199c3ff179c46e6d94c573a080ef46ece2cf
Jonathan <jonnymorgan.esq@gmail.com>
2019-02-01 14:58:54 -0800

    re-enabling odd-number check

--- send_th_invites.py
+++ send_th_invites.py
@@ -86,17 +86,17 @@ if __name__ == "__main__":
     print(params)
 
     for e in eligible:
-##     check e[1] to see if it's odd or not, assign condition appropriately
-#         if e[1] % 2 == 1:
-#             condition = params['conditions'][0] #th-invite
+#     check e[1] to see if it's odd or not, assign condition appropriately
+        if e[1] % 2 == 1:
+            condition = params['conditions'][0] #th-invite
 #             print(e[0] + " has an odd id")
-#         else:
-#             condition = params['conditions'][1] #control
+        else:
+            condition = params['conditions'][1] #control
 #             print(e[0] + " has an even id")
 
 #         profile = send_invites(e, random.choice(inviters), random.choice(params['conditions']), params)
-        profile = send_invites(e, random.choice(inviters), "th-invite", params)
-#         profile = send_invites(e, random.choice(inviters), condition, params)
+#         profile = send_invites(e, random.choice(inviters), "th-invite", params)
+        profile = send_invites(e, random.choice(inviters), condition, params)
 
         daily_sample.update_rows(params['status update query'], [profile.condition, int(profile.invited), int(profile.skip), profile.user_id], single_row = True)
 
